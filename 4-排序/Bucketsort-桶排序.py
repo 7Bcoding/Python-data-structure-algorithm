fb5b4bc1ed74c1f03c2090536fd7260a650cb658
7Bcoding <541965325@qq.com>
2020-04-28 17:59:00 +0800

    20200428提交，添加选择排序，冒泡排序更改

---
+++ 4-排序/Bucketsort-桶排序.py
@@ -0,0 +1,21 @@
+import random
+
+
+def bucket_sort(lst):
+    buckets = [0] * ((max(lst) - min(lst))+1)
+    for i in range(len(lst)):
+        buckets[lst[i]-min(lst)] += 1
+    res = []
+    for i in range(len(buckets)):
+        if buckets[i] != 0:
+            res += [i+min(lst)]*buckets[i]
+    return res
+
+
+if __name__ == '__main__':
+    a = [random.randint(0, 999) for i in range(30)]
+    print("排序前...")
+    print(a)
+    a = bucket_sort(a)
+    print("排序后...")
+    print(a)
